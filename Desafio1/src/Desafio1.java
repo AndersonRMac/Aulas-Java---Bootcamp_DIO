598154fa668ba5c3f4afb78bcb4d988b7aea4098
AndersonRMac <anderson.ramosmaciel@gmail.com>
2022-02-22 17:42:13 -0300

    commit inicial

---
+++ Desafio1/src/Desafio1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner; 
+
+
+public class Desafio1{
+	public static void main(String[] args){
+	  
+		Scanner scan = new Scanner(System.in);
+
+		 double pi = 3.14159;
+		 double area;
+		 double A;
+		 
+		 
+		 area = scan.nextDouble();
+
+		 A = 3.14159 * (Math.pow( area ,2));
+
+		System.out.printf("A=%.4f ", A);
+	}
+}
