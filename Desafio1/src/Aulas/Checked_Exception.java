d14778e973e73825b8caebb99717e8ec77b527b7
AndersonRMac <anderson.ramosmaciel@gmail.com>
2022-03-30 20:39:27 -0300

    finalização da aulda "Checked Exceptions"

--- Desafio1/src/Aulas/Checked_Exception.java
+++ Desafio1/src/Aulas/Checked_Exception.java
@@ -1,23 +1,44 @@
 package Aulas;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
+import javax.swing.*;
+import java.io.*;
 
 public class Checked_Exception {
     public static void main(String[] args) {
 
-        String nomeDoArquivo = "familia-maciel.txt";
+        String nomeDoArquivo = "fmilia-maciel.txt";
 
 //está em negrito por conta do checked exception//
-    imprimirArquivoNoConsole(nomeDoArquivo);
-
-    System.out.println("Apesar da exception ou não, o programa continua...");
-}
-
-        public static void imprimirArquivoNoConsole(String nomeDoArquivo) {
-            File file = new File(nomeDoArquivo);
-            
+        try {
+            imprimirArquivoNoConsole(nomeDoArquivo);
+        } catch (FileNotFoundException f){
+            JOptionPane.showMessageDialog(null, "Arquivo não encontrado");
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null,"Ocorreu um erro inesperado, entre em contato com o suporte." + e.getCause());
+        } finally {
+            System.out.println("Chegou no finally");
         }
-    }
+
+        System.out.println("Apesar da exception ou não, o programa continua...");
 }
+
+        public static void imprimirArquivoNoConsole(String nomeDoArquivo) throws IOException {
+            File file = new File(nomeDoArquivo);
+
+            BufferedReader br = new BufferedReader(new FileReader(file.getName()));
+            String line = br.readLine();
+
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+            do {
+                bw.write(line);
+                bw.newLine();
+                line = br.readLine();
+
+            }while (line !=null);
+            bw.flush();
+            br.close();
+
+        }
+}
+
