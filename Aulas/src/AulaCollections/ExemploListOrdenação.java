3aca3cee99e7a82fcf74bff0727b7d1b0af40d07
anderson.ramosmaciel@gmail.com <And3rson123@>
2022-04-25 11:02:56 -0300

    Aulas de Collections 25/04/2022

--- Aulas/src/AulaCollections/ExemploListOrdenação.java
+++ Aulas/src/AulaCollections/ExemploListOrdenação.java
@@ -7,9 +7,38 @@ Gato 1 = nome: Simba, idade: 6, cor: tigrado
 Gato 1 = nome: Jon, idade: 12, cor: amarelo
  */
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ExemploListOrdenação {
     public static void main(String[] args) {
 
 
+
+
+    }
+}
+
+class Gato{
+    private String nome;
+    private Integer idade;
+    private String cor;
+
+    public Gato(String nome, Integer idade, String cor) {
+        this.nome = nome;
+        this.idade = idade;
+        this.cor = cor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public String getCor() {
+        return cor;
     }
 }
