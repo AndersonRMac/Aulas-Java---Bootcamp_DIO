5bac6f8c090fb62d31c4bde71a8fb937a6316f34
anderson.ramosmaciel@gmail.com <And3rson123@>
2022-04-25 10:46:09 -0300

    Aulas de Collections 25/04/2022

--- Aulas/src/AulaCollections/ExemploList.java
+++ Aulas/src/AulaCollections/ExemploList.java
@@ -1,7 +1,6 @@
 package AulaCollections;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class ExemploList {
     public static void main(String[] args) {
@@ -37,6 +36,44 @@ public class ExemploList {
 
         /*System.out.println("Exiba todas as notas na ordem em que foram informadas: ");
         for (Double nota : notas) System.out.println(nota);*/
+
+        System.out.println("Exiba a terceira nota adicionada: " + notas.get(2));
+        System.out.println(notas.toString());
+
+        System.out.println("Exiba a menor nota: " + Collections.min(notas));
+        System.out.println("Exiba a maior nota: " + Collections.max(notas));
+
+        Iterator<Double> iterator = notas.iterator();
+        Double soma = 0d;
+        while (iterator.hasNext()){
+            Double next = iterator.next();
+            soma = soma + next;
+        }
+        System.out.println("Exiba a soma dos valores: " + soma); // A resposta deste está na logica acima com o Iterator//
+        System.out.println("Exiba a média das notas: " + soma/notas.size()); //o método size retorna a quantidade de valores que contem na lista de notas//
+        System.out.println("Remova a nota 0 " + notas.remove(6));
+        System.out.println(notas.toString());
+
+        System.out.println("Remova a nota na posição 0 " + notas.remove(0));
+        System.out.println(notas.toString());
+
+        System.out.println("Remova as notas menores que 7 e exiba na lista: ");
+        Iterator<Double> iterator1 = notas.iterator();
+        while (iterator1.hasNext()){
+            Double next = iterator1.next();
+            if (next < 7 ) iterator1.remove();
+        }
+        System.out.println(notas);
+
+        System.out.println("Apague toda a lista: ");
+        notas.clear();
+        System.out.println(notas);
+
+
+
+
+
+
 
 
 
---
+++ Aulas/src/AulaCollections/ExemploListOrdenação.java
@@ -0,0 +1,15 @@
+package AulaCollections;
+
+/*Dadas as informações abaixo. Crie uma lista e ordene esta lista exibindo: Nome - Idade - Cor;
+
+Gato 1 = nome: Jon, idade: 18, cor: preto
+Gato 1 = nome: Simba, idade: 6, cor: tigrado
+Gato 1 = nome: Jon, idade: 12, cor: amarelo
+ */
+
+public class ExemploListOrdenação {
+    public static void main(String[] args) {
+
+
+    }
+}
